e42a3060ff0e8e0e06ff20711140a7bf0135bc19
SairamReddy96 <135652120+SairamReddy96@users.noreply.github.com>
2024-09-24 23:39:13 +0530

    Web Version added
    - dynamically move lava
    - added Web Version

--- Assets/Scripts/LavaMovement.cs
+++ Assets/Scripts/LavaMovement.cs
@@ -1,9 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LavaMovement : MonoBehaviour
 {
     [SerializeField] private float movSpeed = 10.0f;
     private GameManager gameManager;
+    [SerializeField] private Transform player;
+    [SerializeField] private float buffDist = 75f;
+    [SerializeField] private float offset = 25f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,9 +22,21 @@ public class LavaMovement : MonoBehaviour
     }
     void MoveUp()
     {
+        Vector3 playerPos = player.position;
         Vector3 currPos = transform.position;
-        Vector3 targetPos = currPos + new Vector3(0, 1f, 0);
+        Vector3 targetPos = new Vector3(transform.position.x, playerPos.y - offset, transform.position.z);
 
-        transform.position = Vector3.Lerp(currPos, targetPos, movSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(currPos, targetPos, getLavaVelocity(playerPos, transform.position) * Time.deltaTime);
+        Debug.Log("The lavaspeed is " + getLavaVelocity(playerPos, transform.position));
+    }
+    float getLavaVelocity(Vector3 playerPos, Vector3 lavaPos)
+    {
+        float lavaSpeed = 0.5f;
+        float distance = Vector3.Distance(lavaPos, playerPos);
+        if(distance > buffDist)
+        {
+            return lavaSpeed * 3f;
+        }
+        return lavaSpeed;
     }
 }
